996cde8986544113f323fff15ddc8615e778e8ca
Morad <morad.labrid@laplateforme.io>
2021-08-04 12:15:04 +0200

    ajout la page annonce

---
+++ page/admin/annonces.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Assets/css/adminannonces.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include 'header.html' ?>
+    <main class="AdminAnnonces">
+        <section>
+            <article>
+                <div>
+                    <img src="../../Assets/img/test.png" alt="">
+                </div>
+                <div>
+                    <h5>Appartement Marseille -Secteur gare T3 de 68m + balcon terasse</h5>
+                    <h4>678€</h4>
+                    <p>Apparetemnt <br> Marseille <br>13/07/2021 13h27</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="../../Assets/img/test.png" alt="">
+                </div>
+                <div>
+                    <h5>Appartement Marseille -Secteur gare T3 de 68m + balcon terasse</h5>
+                    <h4>678€</h4>
+                    <p>Apparetemnt <br> Marseille <br>13/07/2021 13h27</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="../../Assets/img/test.png" alt="">
+                </div>
+                <div>
+                    <h5>Appartement Marseille -Secteur gare T3 de 68m + balcon terasse</h5>
+                    <h4>678€</h4>
+                    <p>Apparetemnt <br> Marseille <br>13/07/2021 13h27</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="../../Assets/img/test.png" alt="">
+                </div>
+                <div>
+                    <h5>Appartement Marseille -Secteur gare T3 de 68m + balcon terasse</h5>
+                    <h4>678€</h4>
+                    <p>Apparetemnt <br> Marseille <br>13/07/2021 13h27</p>
+                </div>
+            </article>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
--- page/admin/valideannonce.php
+++ page/admin/valideannonce.php
@@ -5,22 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Assets/css/header.css">
+    <link rel="stylesheet" href="../../Assets/css/valideannonce.css">
     <title>Document</title>
 </head>
 
 <body>
-    <header></header>
-    <main>
+    <?php 
+    include "header.html";
+    ?>
+    <main class="addAnnonces">
         <section>
             <div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div><img src="../../Assets/img/test.png" alt=""></div>
+                <div class="img">
+                    <img src="../../Assets/img/test.png" alt="">
+                    <img src="../../Assets/img/test.png" alt="">
+                </div>
             </div>
             <div>
                 <form action="" method="post">
-                    <input type="text" name="" id="">
-                    <input type="number" name="" id="">
+                    <input type="text" name="" id=""><br>
+                    <input type="number" name="" id=""><br>
                     <textarea name="" id="" cols="30" rows="10"></textarea>
                 </form>
             </div>
